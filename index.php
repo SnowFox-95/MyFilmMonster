5418cc1d72bca0b66394ee7823a01b8aef1e36b8
SnowFox-95 <321mts95@gmail.com>
2022-12-09 16:49:29 +0300

    php-course: переменные

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-echo "test";
+
+$name = "Alex";
+$balance = 0;
+$addBalance1 = 10;
+$addBalance2 = 35;
+
+$balance = $addBalance1 + $addBalance2;
+echo $balance;
+
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<div> Привет,<?php //echo +$name; ?></div> <!--Внедрение php в html-->
+<?php echo "<div>" . $balance . "</div>"; ?> <!--Внедрение html в php-->
+</body>
+</html>
+
