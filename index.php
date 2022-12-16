dd8589b03e2e2937865f56978aba46915684226b
SnowFox-95 <321mts95@gmail.com>
2022-12-16 17:45:18 +0300

    ветвления: switch

--- index.php
+++ index.php
@@ -10,12 +10,25 @@ $addBalance2 = 35;
 $balance = $addBalance1 + $addBalance2;
 
 $light = "green"; /*Переменная светофор = зеленый/красный*/
+/*
 if ($light == "green") {
     echo "Светофор зеленый, я перехожу дорогу";
 } else if($light=="yellow") {
     echo "Сейчас светофор желтый";
 } else {
     echo "Жду когда загорится зеленый";
+}
+ */
+switch($light){
+    case 'green':
+        echo "Светофор зеленый, я перехожу дорогу";
+        break;
+    case 'yellow':
+        echo "Сейчас светофор желтый";
+        break;
+    default:
+        echo "Жду когда загорится зеленый";
+        break;
 }
 
 ?>
