464d619986d64851ccf45312c54e0d241d6eb980
SnowFox-95 <321mts95@gmail.com>
2022-12-16 17:55:26 +0300

    php-course: цикл while

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <?php
 $i = 1;
 
-/*while($i<=10) {
+while($i<=10) {
     echo $i."<br>";
     $i++;
-}*/
-
-/*do{
-    echo $i."<br>";
-    $i++;
-} while ($i<=10);*/
-
-for($i=0; $i<=10; $i++){
-    echo $i."<br>";
 }
 
 ?>
