0c4ff75588c5f466c15da239643d877ccc772ce1
SnowFox-95 <321mts95@gmail.com>
2023-02-07 23:47:55 +0300

    php-course: массивы. многомерные массивы

--- index.php
+++ index.php
@@ -31,22 +31,40 @@ $arr = array_unique($arr); /*удаление дубликатов*/
 
 //shuffle($arr); /*перемешивание значений в случ. порядке*/
 
-$arr2= array("кокос","арбуз");
-$newarr = array_merge($arr,$arr2); /*объединение 2 массивов в один*/
+$arr2 = array("кокос", "арбуз");
+$newarr = array_merge($arr, $arr2); /*объединение 2 массивов в один*/
 
 foreach ($newarr as $key => $value) {
     echo "Ключ к массиву " . $key . " - Значение массива " . $value . "<br>";
 }
 
 
-$str= implode(", ", $newarr); /*перевод массива в строку*/
-echo "<br>".$str;
+$str = implode(", ", $newarr); /*перевод массива в строку*/
+echo "<br>" . $str;
 echo "<br>";
 $str2 = "продукты, бутылка с водой, кокос";
-$str2toarr = explode(", ",$str2);
+$str2toarr = explode(", ", $str2); /*перевод строки в массив*/
 echo "<pre>"; /*тег для удобного вывода массива через print_r*/
 print_r($str2toarr);
 echo "</pre>";
+
+/*многомерные массивы*/
+$camera["market1"]["row1"][0] = "продукты";
+$camera["market1"]["row1"][1] = "сумка";
+$camera["market2"]["row2"][0] = "апельсин";
+$camera["market2"]["row2"][1] = "фотоаппарат";
+$camera["market2"]["row2"][2] = "телефон";
+echo "<pre>";
+print_r($camera);
+echo "</pre>";
+
+foreach ($camera as $key => $value) {
+    foreach ($value as $key2 => $value2) {
+        foreach ($value2 as $key3 => $value3) {
+            echo $value3;
+        }
+    }
+}
 ?>
 
 <!doctype html>
