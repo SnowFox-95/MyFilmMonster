bbf9f118143c9da6be9d62cd3046d87153fd74c5
SnowFox-95 <321mts95@gmail.com>
2023-02-08 09:57:21 +0300

    php-course: функции. создание своей функции

--- index.php
+++ index.php
@@ -1,40 +1,16 @@
 <?php
-//print_r($_GET);
-
-//echo $_GET['name'];
-//print_r($_POST);
-
-if (isset($_POST['name'])) {
-    $nameFilter = htmlspecialchars($_POST['name'],ENT_QUOTES, 'UTF-8'); //простая защита от xss-атаки
-    echo "Мое имя," . $nameFilter;
+function sum($a, $b)
+{
+    $result = false;
+    if(!is_numeric($a)){
+        $result= "Ошибка. Значение 'a'  не является числом";
+    } else if(!is_numeric($b)){
+        $result= "Ошибка. Значение 'b'  не является числом";}
+    else{
+        $result = $a+$b;
+    }
+    echo $result;
 }
 
-//$user = "    uSeR   ";
-//echo trim($user); //удаление лишних пробелов
-//echo mb_strtolower(trim($user)); //приведение строки в строчный вариант + удаление пробелов
-
-
+echo sum('2', 'you');
 ?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-<!--
-<form action="/" method="get">
-    <input type="text" name="name" placeholder="Ваше имя">
-    <input type="submit" value="отправить">
-</form>
--->
-<form action="/" method="post">
-    <input type="text" name="name" placeholder="Ваше имя">
-    <textarea name="comment"></textarea>
-    <input type="hidden" name="id_user" value="1239">
-    <input type="submit" value="отправить">
-</form>
-</body>
-</html>
-
