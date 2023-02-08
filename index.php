14614be9d5f6e1b51daa104e525f9892faa38fdb
SnowFox-95 <321mts95@gmail.com>
2023-02-08 10:03:54 +0300

    php-course: функции. взаимодействие созданных функций

--- index.php
+++ index.php
@@ -2,15 +2,26 @@
 function sum($a, $b)
 {
     $result = false;
-    if(!is_numeric($a)){
-        $result= "Ошибка. Значение 'a'  не является числом";
-    } else if(!is_numeric($b)){
-        $result= "Ошибка. Значение 'b'  не является числом";}
-    else{
-        $result = $a+$b;
+    if (!is_numeric($a)) {
+        $result = "Ошибка. Значение 'a'  не является числом";
+    } else if (!is_numeric($b)) {
+        $result = "Ошибка. Значение 'b'  не является числом";
+    } else {
+        $result = $a + $b;
     }
     echo $result;
 }
 
-echo sum('2', 'you');
+function maxSum($sum)
+{
+    $result = false;
+    if ($sum > 30) {
+        $result = "Сумма больше 30";
+    } else {
+        $result = "Сумма меньше 30";
+    }
+    return "<br>".$result;
+}
+
+echo Maxsum(sum(2, 5));
 ?>
